815b2d0d37a43a2469524b6c461e87d66db5c857
Neil Kuan <guan840912@gmail.com>
2023-11-19 12:05:33 +0800

    release: 0.0.6

---
+++ get-helm-depends-update.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env zx
+const doc = YAML.parse(fs.readFileSync('./Chart.lock', 'utf8'));
+let repoList = [['helm-demo', process.env.AWS_S3]];
+doc.dependencies.forEach( repo => {
+  repoList.push([repo.name, repo.repository]);
+});
+
+const { spawn } = require("child_process");
+for (const repo of repoList) {
+  let helmAddRepo = spawn('helm', [...['repo', 'add'],...repo]);
+  const helmActionList = [ helmAddRepo];
+  for (const action of helmActionList) {
+      action.stdout.on("data", data => {
+         console.log(`stdout: ${data}`);
+      });
+  
+      action.stderr.on("data", data => {
+          console.log(`stderr: ${data}`);
+      });
+      
+      action.on("close", code => {
+          console.log(`child process exited with code ${code}`);
+      });
+  }
+}
